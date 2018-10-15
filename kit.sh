3cd6af3aaeda4acd8c2e491efc318df9e4c11cd3
Deni Cho <dencho.12@gmail.com>
2018-10-15 23:34:55 +0700

    Deployment

--- kit.sh
+++ kit.sh
@@ -8,6 +8,14 @@ elif [ "$1" = "upbuild" ]
 then
     echo "Building docker development server..."
     docker-compose -f docker-compose-dev.yml up --build
+elif [ "$1" = "deploy" ]
+then
+    echo "Switching env to production..."
+    eval $(docker-machine env forexapp-prod)
+    echo "Building & deploying..."
+    docker-compose -f docker-compose-prod.yml up -d --build
+    echo "Success... switching to unset env"
+    eval $(docker-machine env -u)
 else
     echo "Nothing to do..."
 fi
